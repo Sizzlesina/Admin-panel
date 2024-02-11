c05d421ac40a713aae279117433d8ba1902ad5b4
Sizzlesina <www.sins604@gmail.com>
2024-02-11 09:28:35 +0330

    React

---
+++ react/vite.config.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-react";
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), eslint()],
+});
